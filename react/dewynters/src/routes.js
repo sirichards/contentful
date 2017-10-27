a2dc3d91f0cf97a2c75f4e255cb8ebdd115c44ce
Simon Richards <s.richards@dewynters.com>
2017-10-27 22:48:09 +0100

    router changes

--- react/dewynters/src/routes.js
+++ react/dewynters/src/routes.js
@@ -1,22 +1,18 @@
 // src/routes.js
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import App from './components/App';
 import About from './components/About';
 import NotFound from './components/NotFound';
 
-// import NotFound from './components/NotFound';
-
-    //<Route path="/about" component={About} />
-    //
 const Routes = () => {
     return (
-    	<div>
+    	<Switch>
 			<Route exact path="/" component={App} />
 			<Route path="/about" component={About} />
 			<Route path="*" component={NotFound} />
-		</div>
+		</Switch>
     );
 }
 
